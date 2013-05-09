36e5a75d1b2e5bb3c47d7fb13401876b8227e904
angel <angel@angel>
2013-05-09 16:19:54 -0430

    instalacion

---
+++ Proyecto/instalacion.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#para poner en el crontab una linea cada 5 minutos
+#crontab -l ; echo "0 4 * * * myscript"| crontab - 
+
+
+sshpass -p 'xubuntu' scp -o StrictHostKeyChecking=no /home/angel/Escritorio/Proyecto/distribuidos/Proyecto/nuevo.py xubuntu@10.42.0.40:/home/xubuntu/Escritorio
+
+sshpass -p 'xubuntu' ssh xubuntu@10.42.0.40 'echo "*/5 * * * * /home/xubuntu/Escritorio/nuevo.py" | crontab -'
+
+
+
+
+
+#sshpass -p 'xubuntu' ssh -o StrictHostKeyChecking=no xubuntu@10.42.0.37
+
+
+
+
+
